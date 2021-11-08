85837010f5d7d5d5a27055d92e5eb84137491129
Jesus Mejia <chucho4720013@gmail.com>
2021-11-08 11:31:54 -0500

    State

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import '../styles/TodoSearch.css'
+import React from "react";
+import "../styles/TodoSearch.css";
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState("");
+
   const onSearchValueChange = (event) => {
-    console.log(event.target.value)
-  }
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
   return (
-    <input className="TodoSearch" placeholder="cebolla" onChange={onSearchValueChange}/>
-  )
+    <input
+      className="TodoSearch"
+      placeholder="write a word"
+      value={searchValue}
+      onChange={onSearchValueChange}
+    />
+  );
 }
 
-export { TodoSearch }
+export { TodoSearch };
