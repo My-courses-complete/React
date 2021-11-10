a961ce6d5cd61e58c249ba068aa478fcf15c6b97
Jesus Mejia <chucho4720013@gmail.com>
2021-11-10 17:26:06 -0500

    Effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
 // import "./App.css";
+import React from "react";
 import { TodoCounter } from "./components/TodoCounter.jsx";
 import { TodoSearch } from "./components/TodoSearch.jsx";
 import { TodoList } from "./components/TodoList.jsx";
 import { TodoItem } from "./components/TodoItem.jsx";
 import { CreateTodoButton } from "./components/CreateTodoButton.jsx";
-import React from "react";
-
-function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
-
-  if (!localStorageItem) {
-    localStorage.setItem("TODOS", JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
-  const [item, setItem] = React.useState(parsedItem);
-
-  const saveItem = (newItem) => {
-    localStorage.setItem(itemName, JSON.stringify(newItem));
-    setItem(newItem);
-  };
-
-  return [item, saveItem];
-}
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 function App() {
-  const [todos, saveTodos] = useLocalStorage("TODOS", []);
+  const {item: todos, saveItem: saveTodos, loading, error} = useLocalStorage("TODOS", []);
   const [searchValue, setSearchValue] = React.useState("");
 
   const completedTodos = todos.filter((todo) => todo.completed).length;
@@ -56,11 +37,18 @@ function App() {
     saveTodos(newTodos);
   };
 
+  React.useEffect(() => {
+    saveTodos(todos);
+  }, [totalTodos]);
+
   return (
     <>
       <TodoCounter total={totalTodos} completed={completedTodos} />
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
+        {error && <p>Hubo un error</p>}
+        {loading && <p>Cargando...</p>}
+        {(!loading && !searchedTodos.length) && <p>Crea tu primer TODO!</p>}
         {searchedTodos.map((todo, index) => (
           <TodoItem
             key={index}
