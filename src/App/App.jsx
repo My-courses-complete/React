62a8f1b9c54b331e39d799efcd0611e1f9a85310
Jesus Mejia <chucho4720013@gmail.com>
2022-01-03 00:23:03 -0500

    composition in state

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,13 +4,55 @@ import { AppUI } from "./AppUI";
 import { TodoContextProvider } from "../context/TodoContext";
 
 function App() {
+  const [state, setState] = React.useState('Estado compartido');
   return (
     <>
-      <TodoContextProvider >
-        <AppUI />
-      </TodoContextProvider>
+    <TodoHeader>
+      <TodoSearch />
+      <TodoCounter />
+    </TodoHeader>
+    <TodoList>
+      <TodoItems state={state}/>
+    </TodoList>
     </>
   );
 }
+function TodoHeader({ children }) {
+  return (
+    <header>
+      {children}
+    </header>
+  );
+}
+
+function TodoList({ children }) {
+  return (
+    <section className="TodoList-container">
+      {children}
+    </section>
+  );
+}
+
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+
+function TodoItems({ state }) {
+  return <p>TodoItems {state}</p>;
+}
+
+// function App() {
+//   return (
+//     <>
+//       <TodoContextProvider >
+//         <AppUI />
+//       </TodoContextProvider>
+//     </>
+//   );
+// }
 
 export { App };
