3e3bdabf6c4d3426ad8f64c55f5427f423feb549
Jesus Mejia <chucho4720013@gmail.com>
2022-01-09 03:23:36 -0500

    HOC

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { App } from "./App/App";
+// import { App } from "./App/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function App(props) {
+	return (
+		<h1>{props.saludo}, {props.nombre}</h1>
+	);
+	}
+
+function withSaludo(WrappedComponent) {
+	return function wrapped() {
+		return function componente(props) {
+			return (
+				<>
+					<WrappedComponent {...props}/>
+					<p>Acompaño al wrapped</p>
+				</>
+			)
+		}
+	}
+}
+
+const AppWithWhatever = withSaludo(App)();
+ReactDOM.render(<AppWithWhatever saludo="hey" nombre="Jesus" />, document.getElementById("root"));
